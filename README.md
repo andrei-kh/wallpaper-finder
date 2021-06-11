328e7b5c9fd6e3187955b0383829591b06c37956
andreywastaken <andreihombak@gmail.com>
2021-06-12 02:27:20 +0300

    main code clean up. some fixes. readme rework.

--- README.md
+++ README.md
@@ -1,17 +1,143 @@
 # WALLPAPER FINDER
-Finds wallpapers on reddit and saves them.
+![Example](https://imgur.com/qMSnoyR.png)
 
-# TODO
-- Reddit api part
-  - [ ] make basic stuff
-  - [ ] Image checks
-  - [ ] Settings
-  - [ ] Clean up
-- General things
-  - [x] Image viewer
-    - [x] Clean up code 
-    - [x] Ability to pick pictures
-    - [x] Add json for settings
-    - [ ] Help label?
-  - [ ] Image comparer
-  - [ ] libpng error
+A simple app that lets you download wallpapers from reddit.- 
+
+- [WALLPAPER FINDER](#wallpaper-finder)
+  - [Installation](#installation)
+  - [Usage](#usage)
+    - [Image viewer controls:](#image-viewer-controls)
+  - [Image Viewer settings](#image-viewer-settings)
+
+## Installation
+Make sure you have python 3.x installed.
+
+1. Clone and install requirements
+```sh
+> git clone https://github.com/andreywastaken/wallpaper-finder.git
+> cd wallpaper-finder
+
+> pip install -r requirements.txt
+```
+
+2. Get reddit api access
+     1. Go to [reddit app preferences](https://www.reddit.com/prefs/apps) and click __create another app...__
+   
+     2. Fill out the required details, select __script__ and click __create an application__
+    ![Reddit application](https://imgur.com/OIvQUQs.png)
+
+     3. __script for personal use__ and __secret__ tokens are used to connect to reddit api.
+    ![Personal and Secret](https://imgur.com/sw6W1Qx.png)
+
+     4. Create in __.secret__ create __credentials.json__ file
+        ```json like that:
+        {
+        "client_id": "aB1cdeFghI23JK",               // short key one goes here
+        "api_key": "LMNopQrst4Uv5Wx67yZOHNOItseNds", // long key one goes here
+        "username": "<your reddit username>",        // reddit username goes her
+        "password": "<your reddit password>"         // reddit password goes here
+        }
+        ```
+     5. You finished with installation. For test you can execute:
+        ```python
+        python wallpaper_finder.py
+        ```
+
+## Usage
+1. Before running
+   
+    You can run ```python wallpaper_finder.py -h``` or ```python wallpaper_finder.py -help``` to see help:
+
+    ```
+    > python .\wallpaper_finder.py -h
+    usage: wallpaper_finder.py [-h] [-s SUBREDDITS [SUBREDDITS ...]] [-st   [TYPE]  ] [-l [LIMIT]]
+                               [-tf [TIME_FILTER]] [-rd] [--credentials [PATH]  ]   [--save-folder [PATH]]
+                               [--temp-folder [PATH]]
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -s SUBREDDITS [SUBREDDITS ...], --subreddits SUBREDDITS [SUBREDDITS ...]
+                            Images would be parsed from these.
+      -st [TYPE], --sort-type [TYPE]
+                            Sort type. Can be hot, new, top, rising.
+      -l [LIMIT], --limit [LIMIT]
+                            How many images would be parsed.
+      -tf [TIME_FILTER], --time-filter [TIME_FILTER]
+                            Only with sort-type top. Top from day, week,  month,   year or all.
+      -rd, --remove-duplicates
+                            Script would not save duplicates of images in     save-folder. If you have a lot of
+                            images you will die before it finishes.
+      --credentials [PATH]  Folder with credentials.json.
+      --save-folder [PATH]  Folder where immages would be saved.
+      --temp-folder [PATH]  Temporary folder to save images. WARNING:   TEMPORARY   FOLDER WOULD BE CLEARED!!!
+    ```
+
+    Also, you can edit __settings.json__, but values passed as a parameter     __override__ values from __settings.json__
+    ```json
+    {
+        "credentials_path": ".secret/credentials.json", // folder with    credentialas.json
+        "save_folder_path": "<better change this>",     // folder where   images  are  saved
+        "temp_folder_path": "./temp",                   // folder where   images  are saved during runtime
+        "subreddits": [                                 // subreddits to parse
+            "wallpaper"
+        ],
+        "sort_type": "top",                             // how to sort    submissions
+        "limit": 10,                                    // how many   submissions   will be loaded
+        "time_filter": "month",                         // top from   'time_filter'
+        "remove_duplicates": false                      // checks for   duplicates
+    }
+    ```
+2. After executing script will load the images:
+   ![Image loading](https://imgur.com/SWpOmzt.png)
+
+3. Image viewer will open after all images are open:
+   ![Image Viewer](https://imgur.com/KBwex7c.png)
+
+   You can pick images you want to save with __ALT+X__ shortcut.
+
+   All shortcuts:
+   ### Image viewer controls:
+   | Button            | Action          |
+   | ----------------- | --------------- |
+   | __Right arrow__   | next image      |
+   | __Left arrow__    | previous image  |
+   | __ESC or CTRL+Q__ | close viewer    |
+   | __ALT+X__         | pick image      |
+   | __CTRL+O__        | open files      |
+   | __CTRL+F__        | maximize window |
+   | __CTRL+R__        | reload window   |
+
+4. After that chosen images would be saved to ```"save_folder"```. If ```-rt``` is present or ```"remove_duplicates"``` is set to ```true``` images that already exist in ```"save_folder"``` would not be saved.
+
+## Image Viewer settings
+Image viewer has it's own ```settings.json``` at ```wallpaper-finder/image_viewer```:
+```json
+{
+    "main_window": {
+        "main_layout_margins": [0, 4, 0, 0], // Borders of the main layout (left, top, right, bottom)
+        "main_layout_spacing": 0,            // Spacing between app widgets
+
+        "background": "black",               // Main window background color 
+        "border_size": 6,                    // Main window border size in px
+        "border_color": "white"              // Main window border color
+    },
+
+    "bottom_bar": {
+        "bottom_bar_margins": [0, 0, 0, 0],  // Borders of the bottom bar layout
+        "bottom_bar_spacing": 0,             // Spacing between bottom bar widgets
+        "bottom_bar_height": 28,             // Bottom bar height in px
+
+
+        "picker_symbol": "🗹",               // Symbol that shows when image is picked
+        "picker_color": "black",             // Color of the symbol 
+
+        "background": "gray",                // Bottom bar background
+        "top_border_size": 2,                // Bottom bar top border size in px
+        "top_border_color": "white",         // Bottom bar top border color
+
+        "font_size": "11",                   // Bottom bar font
+        "font_weight": "bold",               // Bottom bar font weight
+        "font_family": "Consolas"            // Bottom bar font family
+    }
+}
+```
---
+++ wallpaper_finder.py
@@ -0,0 +1,348 @@
+import praw
+
+from PIL import Image, ImageChops
+
+import json
+
+import os
+import shutil
+
+from urllib.parse import urlparse
+import requests
+
+from progress.bar import Bar
+
+from typing import Optional
+
+import argparse
+
+from image_viewer import ImageViewer
+
+USER_AGENT = "Wallpaper finder"
+SETTINGS_PATH = "./settings.json"
+
+
+class RedditPictures:
+    def __init__(self, credentials, save_folder, temp_folder="temp",
+                 subreddits=["wallpaper"], sort_type="top", limit=10,
+                 time_filter="month", remove_duplicates=False) -> None:
+        """
+        Used to parse and save images from reddit.
+        """
+        self.temp_folder_path = temp_folder
+        if not os.path.isdir(self.temp_folder_path):
+            os.mkdir(self.temp_folder_path)
+
+        self.save_folder = save_folder
+
+        self.check_for_duplicates = remove_duplicates
+
+        self.reddit = praw.Reddit(client_id=credentials['client_id'],
+                                  client_secret=credentials['api_key'],
+                                  password=credentials['password'],
+                                  user_agent=USER_AGENT,
+                                  username=credentials['username'])
+
+        self.allowed_extensions = ["png", "jpg", "jpeg"]
+
+        self.subreddits = subreddits
+        self.sort_type = sort_type
+        self.limit = limit
+        self.time_filter = time_filter
+
+    def save_image_from_url(self, url, name) -> None:
+        """
+        Saves image.
+        """
+        image = Image.open(requests.get(url, stream=True).raw)
+        image.save(os.path.join(self.temp_folder_path, name), icc_profile='')
+
+    def process_image_url(self, image_url) -> bool:
+        """
+        Checks if images has valid extension and saves it.
+        """
+        image_name = urlparse(image_url).path.split('/')[-1]
+
+        if image_name.split('.')[-1] in self.allowed_extensions:
+            self.save_image_from_url(image_url, image_name)
+            return True
+
+        return False
+
+    def process_gallery(self, submission) -> None:
+        """
+        Procceses the pictures in gallery.
+        """
+        gallery_items = list(submission.gallery_data['items'])
+        media_metadata = submission.media_metadata
+
+        for item in gallery_items:
+            image_url = media_metadata[item['media_id']]['s']['u']
+            self.process_image_url(image_url)
+
+    def get_submissions(self, subreddit_name) -> Optional[praw.models.listing.generator.ListingGenerator]:
+        """
+        Chooses submissions.
+        """
+
+        subreddit = self.reddit.subreddit(subreddit_name)
+
+        if self.sort_type == "top":
+            return subreddit.top(self.time_filter, limit=self.limit)
+        elif self.sort_type == "hot":
+            return subreddit.hot(limit=self.limit)
+        elif self.sort_type == "new":
+            return subreddit.new(limit=self.limit)
+        elif self.sort_type == "rising":
+            return subreddit.rising(limit=self.limit)
+
+        return None
+
+    def load_pictures(self, subreddits) -> None:
+        """
+        Loads images from subreddits.
+        """
+        for subreddit in subreddits:
+            submissions = self.get_submissions(subreddit)
+
+            progress_bar = Bar(f"Loaded images from r/{subreddit}:", max=submissions.limit)
+
+            for submission in submissions:
+                if not self.process_image_url(submission.url):
+                    try:
+                        if submission.is_gallery:
+                            self.process_gallery(submission)
+                    except AttributeError:
+                        print("submission:"
+                              "\n{}\n"
+                              "dosen't have images with allowed extentions\n".format(submission.url))
+
+                progress_bar.next()
+
+            progress_bar.finish()
+
+    def clear_folder(self, folder_path) -> None:
+        """
+        Clears 'folder_path'.
+        """
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+
+    def get_files_from_folder(self, folder_path) -> list:
+        """
+        Returns list of filepaths to files in 'folder_path'.
+        """
+        files = []
+
+        for name in os.listdir(folder_path):
+            full_path = os.path.join(folder_path, name)
+
+            if os.path.isfile(full_path):
+                files.append(full_path)
+
+        return files
+
+    def move_images(self, paths, folder) -> None:
+        """
+        Moves images with 'paths' to 'folder'.
+        """
+        # for already existing files
+        files_existed = []
+
+        if paths:
+            progress_bar = Bar("Moving picked images:", max=len(paths))
+
+            for path in paths:
+                try:
+                    file_name = os.path.basename(path)
+
+                    destination = os.path.join(folder, file_name)
+
+                    os.rename(path, destination)
+
+                    progress_bar.next()
+                except FileExistsError:
+                    files_existed.append(path)
+                    progress_bar.next()
+                    continue
+
+            progress_bar.finish()
+
+            # if we got already existing files -> print them
+            if files_existed:
+                print()
+                for path in files_existed:
+                    print(f"File {path} already exists. Did not move.")
+
+    def remove_duplicates(self, images, folder) -> list:
+        """
+        Returns list of paths without imagess from 'folder'.
+        """
+        bar_lenght = len(images)
+        progress_bar = Bar("Checking if duplicate:", max=bar_lenght)
+
+        duplicates = []
+        for image_path in images:
+            image = Image.open(image_path)
+
+            for filename in os.listdir(folder):
+                file_path = os.path.join(folder, filename)
+                s_image = Image.open(file_path)
+
+                try:
+                    diff = ImageChops.difference(image, s_image)
+                except ValueError:
+                    continue
+
+                if not diff.getbbox():
+                    duplicates.append((image_path, file_path))
+                    break
+
+            progress_bar.next()
+
+        print()
+        for duplicate, original in duplicates:
+            print("{} is a duplicate of {}".format(os.path.basename(duplicate), original))
+
+        duplicates = [dup for dup, _ in duplicates]
+        return [im for im in images if im not in duplicates]
+
+    def run(self) -> None:
+        """
+        Driver code.
+        """
+
+        self.clear_folder(self.temp_folder_path)
+
+        self.load_pictures(self.subreddits)
+
+        image_paths = self.get_files_from_folder(self.temp_folder_path)
+        imageViewer = ImageViewer(image_paths, True)
+
+        chosen_images = imageViewer.run()
+
+        if self.check_for_duplicates and chosen_images:
+            images_to_save = self.remove_duplicates(chosen_images, self.save_folder)
+
+            self.move_images(images_to_save, self.save_folder)
+        else:
+            self.move_images(chosen_images, self.save_folder)
+
+        self.clear_folder(self.temp_folder_path)
+
+
+def arguments() -> dict:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-s",
+        "--subreddits",
+        type=str,
+        default=None,
+        nargs="+",
+        help="Images would be parsed from these."
+    )
+
+    parser.add_argument(
+        "-st",
+        "--sort-type",
+        metavar="TYPE",
+        type=str,
+        default=None,
+        nargs='?',
+        help="Sort type. Can be hot, new, top, rising."
+    )
+
+    parser.add_argument(
+        "-l",
+        "--limit",
+        type=int,
+        default=None,
+        nargs='?',
+        help="How many images would be parsed."
+    )
+
+    parser.add_argument(
+        "-tf",
+        "--time-filter",
+        type=str,
+        default=None,
+        nargs='?',
+        help="Only with sort-type top. Top from day, week, month, year or all."
+    )
+
+    parser.add_argument(
+        "-rd",
+        "--remove-duplicates",
+        action="store_true",
+        default=None,
+        dest="remove_duplicates",
+        help="Script would not save duplicates of images in save-folder. "
+             "If you have a lot of images you will die before it finishes."
+    )
+
+    parser.add_argument(
+        "--credentials",
+        metavar="PATH",
+        dest="credentials_path",
+        type=str,
+        default=None,
+        nargs='?',
+        help="Folder with credentials.json."
+    )
+
+    parser.add_argument(
+        "--save-folder",
+        dest="save_folder_path",
+        metavar="PATH",
+        type=str,
+        default=None,
+        nargs='?',
+        help="Folder where immages would be saved."
+    )
+
+    parser.add_argument(
+        "--temp-folder",
+        dest="temp_folder_path",
+        metavar="PATH",
+        type=str,
+        default=None,
+        nargs='?',
+        help="Temporary folder to save images. WARNING: TEMPORARY FOLDER WOULD BE CLEARED!!!"
+    )
+
+    args = parser.parse_args()
+
+    return vars(args)
+
+
+if __name__ == "__main__":
+    args = arguments()
+    print("Starting...")
+
+    with open(SETTINGS_PATH) as f:
+        settings = json.load(f)
+
+    for k, v in args.items():
+        if v is not None:
+            settings[k] = v
+
+    with open(settings["credentials_path"]) as f:
+        credentials = json.load(f)
+
+    redditPictures = RedditPictures(credentials=credentials,
+                                    save_folder=settings["save_folder_path"],
+                                    temp_folder=settings["temp_folder_path"],
+                                    subreddits=settings["subreddits"],
+                                    sort_type=settings["sort_type"],
+                                    limit=settings["limit"],
+                                    time_filter=settings["time_filter"],
+                                    remove_duplicates=settings["remove_duplicates"])
+    redditPictures.run()
+
+    print("Done...")
